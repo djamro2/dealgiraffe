9d1d8cec4ddda03402fcb4878768f1be271158ef
Dan Jamrozik <djamro2@illinois.edu>
2015-04-24 02:08:28 -0500

    mostly css

--- app/app.js
+++ app/app.js
@@ -9,6 +9,8 @@ app.controller('DealgiraffeController', ['$scope', '$http', '$resource',
 	//vm is the exports variable for this controller
 	var vm = this;
 
+	$scope.showExtra = false;
+
 	vm.init = function(){
 
 		$http.get('master_deals.json').success(function(data) {
@@ -19,7 +21,9 @@ app.controller('DealgiraffeController', ['$scope', '$http', '$resource',
     			if(data.deals[i].Items.Item){
     				vm.dealArray[i].Title = data.deals[i].Items.Item.ItemAttributes.Title;
     				vm.dealArray[i].RealPrice = data.deals[i].Items.Item.Offers.Offer.OfferListing.Price.FormattedPrice;
+    				vm.dealArray[i].OriginalPrice = (Number(data.deals[i].Items.Item.Offers.Offer.OfferListing.AmountSaved.Amount) + Number(data.deals[i].Items.Item.Offers.Offer.OfferListing.Price.Amount)) / 100;
     				vm.dealArray[i].ThumbnailImage = data.deals[i].Items.Item.MediumImage.URL;
+    				vm.dealArray[i].ProductDescription = data.deals[i].Items.Item.EditorialReviews.EditorialReview.Content;
     			} else {
     				vm.dealArray[i].invalidItem = true;
     			}
@@ -27,9 +31,14 @@ app.controller('DealgiraffeController', ['$scope', '$http', '$resource',
 		});
 
 		//TEMPDATA
-		vm.pages = [1,2,3,4,5];
-		vm.pagesToClick = vm.pages.reverse();
+		vm.pages = [1,2,3,4,5]; 
+		vm.pagesToClick = [5,4,3,2,1];
 
+	};
+
+	$scope.toggleExtra = function(){
+		vm.showExtra = !vm.showExtra;
+		console.log('toggleExtra was called!');
 	};
 
 	vm.init();
