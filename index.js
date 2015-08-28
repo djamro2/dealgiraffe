d0cad59aea5442450dec7fc66cca0a6f90850f63
djamro2 <djamro2@illinois.edu>
2015-08-27 20:27:42 -0500

    login for admin panel

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 /* global process */
 /* global __dirname */
 
-var express =        require('express'),
-    bodyParser =     require('body-parser'),
+var express        = require('express'),
+    bodyParser     = require('body-parser'),
     HomeController = require('./server/controllers/HomeController'),
-    local_codes =    require('./local_codes'),
+    local_codes    = require('./local_codes'),
+    passport       = require('passport'),
+    LocalStrategy  = require('passport-local').Strategy,
+    mongoose       = require('mongoose'),
+    expressSession = require('express-session'),
+    bCrypt         = require('bcrypt-nodejs'),
     app = express();
     
 var isProduction = false;
@@ -26,17 +31,55 @@ else
     var port = server.address().port;
     console.log('App listening at http://%s:%s', host, port);
   });
+  mongoose.connect('mongodb://localhost/MyDatabase');
 }
 
 //middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(expressSession({
+  secret: 'mySecretKey',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Using the flash middleware provided by connect-flash to store messages in session
+// and displaying in templates
+var flash = require('connect-flash');
+app.use(flash());
+
 app.use('/client', express.static(__dirname + '/client'));
-  
+
+// Initialize Passport
+var initPassport = require('./server/passport/init');
+initPassport(passport);
+
+//routing (will be moved to it's own file later)
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/client/index.html');
 });
 
-//send the message to me (code in HomeController)
+app.get('/admin', function(req, res){
+  res.sendFile(__dirname + '/client/app/views/admin.html');
+});
+
 app.post('/api/sendmessage', HomeController.send);
+
+/* Handle Login POST */
+app.post('/login', passport.authenticate('login', {
+  successRedirect: '/home',
+  failureRedirect: '/',
+  failureFlash : true 
+}));
+
+app.get('/loginFailure', function(req, res, next) {
+  res.send('Failed to authenticate');
+});
+
+app.get('/loginSuccess', function(req, res, next) {
+  res.send('Successfully authenticated');
+});
 
