2e5975c474d8eabb879fe13b7089fa202cb98959
Daniel Jamrozik <djamro2@illinois.edu>
2016-07-06 00:13:37 -0700

    get correct image

--- client/src/lib/parseResponse.js
+++ client/src/lib/parseResponse.js
@@ -3,9 +3,16 @@
 function parseResponse(items) {
     for (var i = 0; i < items.length; i++) {
         items[i].title = items[i].large_data.ItemAttributes.Title;
-        items[i].image = items[i].large_data.LargeImage.URL;
         items[i].productURL = items[i].large_data.DetailPageURL;
         items[i].pageURL = '/product?id=' + items[i].asin;
+
+        if (items[i].large_data.LargeImage) {
+            items[i].image = items[i].large_data.LargeImage.URL;
+        } else if (items[i].large_data.MediumImage) {
+            items[i].image = items[i].large_data.MediumImage.URL;
+        } else {
+            items[i].image = "";
+        }
     }
     return items;
 }
